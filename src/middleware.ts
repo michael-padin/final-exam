c3a0972c1c763d6734f00d51d9e35c08be3ab9a5
Michael Padin <padinmichael201@gmail.com>
2025-01-10 10:10:41 +0800

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth"
 import { NextResponse } from "next/server"
 import authConfig from "./auth.config"
-import { authRoutes, DEFAULT_LOGIN_REDIRECT, protectedRoutes } from "./routes"
 
 const { auth } = NextAuth(authConfig)
 export default auth(async function middleware(req) {
@@ -9,49 +8,8 @@ export default auth(async function middleware(req) {
 	const { pathname } = nextUrl
 	const isLoggedIn = !!req.auth
 	const user = req.auth?.user
-
-	// 2. Check for auth routes
-	const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route))
-
-	if (pathname === "/" && user?.role === "FARMER") {
-		return NextResponse.redirect(
-			new URL(DEFAULT_LOGIN_REDIRECT(user?.role || "CUSTOMER"), nextUrl)
-		)
-	} else if (
-		pathname.startsWith("/dashboard/farmer") &&
-		user?.role === "CUSTOMER"
-	) {
-		return NextResponse.redirect(
-			new URL(DEFAULT_LOGIN_REDIRECT(user?.role || "CUSTOMER"), nextUrl)
-		)
-	}
-
-	if (isAuthRoute) {
-		if (isLoggedIn) {
-			// Redirect to default page if already logged in
-			return NextResponse.redirect(
-				new URL(DEFAULT_LOGIN_REDIRECT(user?.role || "CUSTOMER"), nextUrl)
-			)
-		}
-	}
-
-	// 3. Check for protected routes
-	const isProtectedRoute = protectedRoutes.some((route) =>
-		pathname.startsWith(route)
-	)
-
-	if (isProtectedRoute) {
-		if (!isLoggedIn) {
-			// Redirect to login if not authenticated
-			return NextResponse.redirect(new URL("/login", nextUrl))
-		} else {
-			// Redirect to verify-email page if not verified
-			if (!user?.isEmailVerified) {
-				return NextResponse.redirect(new URL("/verify-email", nextUrl))
-			}
-		}
-	}
-
+	
+	
 	return
 })
 // Supports both a single string value or an array of matchers
